d51166e2d4b64897ccb489952b873fc7037df3ff
supreetpr-kore <supreet.preet@kore.com>
2022-07-28 13:28:29 +0530

    changes in the preprocessor

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,6 +1,7 @@
 import fasttext
 import random
 import sys
+import pandas
 
 class LanguageIdentification:
 
@@ -22,26 +23,31 @@ tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M")
 def translate_m2m(text, lang_id , trans_lang):
     tokenizer.src_lang = lang_id  
     encoded_hi = tokenizer(text, return_tensors="pt")
-    for key in encoded_hi:
-        encoded_hi[key] = encoded_hi[key].cuda()
+    # for key in encoded_hi:
+    #     encoded_hi[key] = encoded_hi[key].cuda()
     generated_tokens = model.generate(**encoded_hi, forced_bos_token_id=tokenizer.get_lang_id(trans_lang))
-    tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    trans_sent = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return trans_sent[0]
+
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
     df = pandas.read_csv(in_file)
     f = open("temp.txt", "w+")
+    LANGUAGE = LanguageIdentification()
     max_len = 5000
     min_len = 10
     df = df[df['body'].map(len) < max_len ]
     df = df[df['body'].map(len) > min_len ]
     for sent in df.body:
         sent = sent.replace("\n"," ")
-        lang_list = ["af" ,"am" ,"ar" ,"ast" ,"az" ,"ba" ,"be" ,"bg" ,"bn" ,"br" ,"bs" ,"ca" ,"ceb" ,"cs" ,"cy" ,"da" ,"de" ,"el" ,"en" ,"es" ,"et" ,"fa" ,"ff" ,"fi" ,"fr" ,"fy" ,"ga" ,"gd" ,"gl" ,"gu" ,"ha" ,"he" ,"hi" ,"hr" ,"ht" ,"hu" ,"hy" ,"id" ,"ig" ,"ilo" ,"is" ,"it" ,"ja" ,"jv" ,"ka" ,"kk" ,"km" ,"kn" ,"ko" ,"lb" ,"lg" ,"ln" ,"lo" ,"lt" ,"lv" ,"mg" ,"mk" ,"ml" ,"mn" ,"mr" ,"ms" ,"my" ,"ne" ,"nl" ,"no" ,"ns" ,"oc" ,"or" ,"pa" ,"pl" ,"ps" ,"pt" ,"ro" ,"ru" ,"sd" ,"si" ,"sk" ,"sl" ,"so" ,"sq" ,"sr" ,"ss" ,"su" ,"sv" ,"sw" ,"ta" ,"th" ,"tl" ,"tn" ,"tr" ,"uk" ,"ur" ,"uz" ,"vi" ,"wo" ,"xh" ,"yi" ,"yo" ,"zh" ,"zu" ]
-        trans_lang = (random.choices(lang_list, weights=(5, 5, 20, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 20, 5, 100, 20, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5), k=1))[0]
+        lang_list = ["af" ,"am" ,"ar"  ,"az"  ,"be" ,"bg" ,"bn"  ,"bs" ,"ca" ,"ceb" ,"cs" ,"cy" ,"da" ,"de" ,"el" ,"en" ,"es" ,"et" ,"fa" ,"ff" ,"fi" ,"fr" ,"fy" ,"ga" ,"gd" ,"gl" ,"gu" ,"ha" ,"he" ,"hi" ,"hr" ,"ht" ,"hu" ,"hy" ,"id" ,"ig"  ,"is" ,"it" ,"ja" ,"jv" ,"ka" ,"kk" ,"km" ,"kn" ,"ko" ,"lb" ,"lg" ,"ln" ,"lo" ,"lt" ,"lv" ,"mg" ,"mk" ,"ml" ,"mn" ,"mr" ,"ms" ,"my" ,"ne" ,"nl" ,"no"  ,"or" ,"pa" ,"pl"  ,"pt" ,"ro" ,"ru"  ,"si" ,"sk" ,"sl" ,"so" ,"sq" ,"sr" ,"su" ,"sv" ,"sw" ,"ta" ,"th" ,"tl"  ,"tr" ,"uk" ,"ur" ,"uz" ,"vi" ,"wo" ,"xh" ,"yi" ,"yo" ,"zh" ,"zu" ]
+        trans_lang = (random.choices(lang_list, weights=(5, 5, 20, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 20, 5, 50, 20, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 20, 5), k=1))[0]
         lang = LANGUAGE.predict_lang(sent)
         lang_id = lang[0][0].split("_")[-1]
-        if not lang_id == "en":
+        if lang_id not in lang_list:
+            text = sent + '\n'
+        elif not trans_lang == "en":
             sent2 = translate_m2m(sent,lang_id=lang_id,trans_lang=trans_lang)
             text = sent + " || " +sent2 +  '\n'
         else:
